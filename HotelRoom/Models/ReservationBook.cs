c7cb1485bebff0436d50415cf7dd4a97440a7a78
Alvin Yanson <yansonalvin29@gmail.com>
2022-04-09 12:58:07 +0800

    Add project files.

---
+++ HotelRoom/Models/ReservationBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoom.Models
+{
+    public class ReservationBook
+    {
+
+        private readonly Dictionary<RoomID, List<Reservation>> _roomToReservations;
+
+
+        public ReservationBook()
+        {
+            _roomToReservations = new Dictionary<RoomID, List<Reservation>>();
+        }
+
+    }
+}
